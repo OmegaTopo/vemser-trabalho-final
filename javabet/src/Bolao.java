f5b9672174c159a4752c57193c963d90236b40c2
Gabriel Kleiman <kleim4n@gmail.com>
2023-02-11 18:48:33 -0300

    CL06: implementado e verificado função de distribuir prêmios e getResultado

--- javabet/src/Bolao.java
+++ javabet/src/Bolao.java
@@ -5,11 +5,21 @@ public class Bolao implements Premio {
     private ArrayList<Apostador> apostadores;
     private boolean finalizado;
     private int cotas;
+    private int pontos = 0;
+
+    public int getPontos() {
+        return pontos;
+    }
+
+    public void setPontos(int pontos) {
+        this.pontos = pontos;
+    }
 
     public Bolao(int cotas) {
         this.setCotas(cotas);
         this.setApostadores(new ArrayList<>());
         this.setApostas(new ArrayList<>());
+        this.setFinalizado(false);
     }
 
     public Bolao() {
@@ -66,7 +76,49 @@ public class Bolao implements Premio {
     }
 
     @Override
-    public boolean distribuirPremio() {
+    public boolean distribuirPremio(Apostador apostador) {
+//        if (!this.isFinalizado()){
+            int pontoPorCota = this.getPontos()/this.getCotas();
+            this.setFinalizado(true);
+            apostador.setPontos(apostador.getPontos() + pontoPorCota);
+            return true;
+//        }
+
+//        if (!this.getApostas().isEmpty()){
+//            int soma = this.getApostas().stream()
+//                    .filter(aposta -> aposta.getFinalizado())
+//                    .mapToInt(Aposta::getPrevisaoPlacar()[0])
+//                    .sum();
+//        }
+//        return false;
+    }
+
+    @Override
+    public boolean distribuirPremio(Bolao bolao) {
         return false;
+    }
+
+    @Override
+    public boolean distribuirPremio() {
+//        Este variação do distribuirPremio tem como função percorrer toda a lista de apostas para solicitar o premio para o bolão
+        ArrayList<Boolean> distribuicaoOK = new ArrayList<>();
+        for (Aposta aposta : this.getApostas()){
+            distribuicaoOK.add(aposta.distribuirPremio(this));
+        }
+//        System.out.println(!distribuicaoOK.contains(false));
+        if (!distribuicaoOK.contains(false)){//Contem falso
+            for (Apostador apostador : this.getApostadores()){
+//                Nesse ponto é invocada a distribuição de pontos para cada apostador do bolão
+                this.distribuirPremio(apostador);
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int getResultado() {
+        return this.getApostas().stream()
+                .mapToInt(Aposta::getResultado)
+                .sum();
     }
 }
