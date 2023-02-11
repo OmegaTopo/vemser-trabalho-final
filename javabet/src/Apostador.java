9dfe1caac3849a7690c91eaec001c64e05770337
Miguel Zanona Krasner <miguelzkestudos@gmail.com>
2023-02-10 22:43:50 -0300

    CL - Apostador em andamento

--- javabet/src/Apostador.java
+++ javabet/src/Apostador.java
@@ -2,14 +2,77 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Apostador extends Usuario implements Imprimir{
-<<<<<<< HEAD
 
-
-=======
     private int pontos;
-    private ArrayList<Aposta> apostas = new ArrayList<>();
-    private ArrayList<Bolao> boloes = new ArrayList<>();
+    private ArrayList<Aposta> apostas;
+    private ArrayList<Bolao> boloes;
     private String nome;
-    private Date
->>>>>>> 2dec6d96404176c08215b5675f7e838375905df6
+    private Date dataNascimento;
+    private String cpf;
+
+    public Apostador(String nome, int dia, int mes, int ano, String cpf, String email, String senha) {
+        super(email, senha);
+        this.nome = nome;
+        this.dataNascimento = new Date(ano, mes, dia);
+        this.cpf = cpf;
+        this.pontos = 0;
+        this.apostas = new ArrayList<>();;
+        this.boloes = new ArrayList<>();;
+    }
+
+    @Override
+    public String toString() {
+        return "Apostador " +
+                "Nome: " + nome + "\n" +
+                "Data de nascimento: " + dataNascimento + "\n" +
+                "CPF: " + cpf + "\n" +
+                "Pontos: " + pontos + "\n" +
+                "Apostas:" + "\n" + apostas + "\n" +
+                "Boloes:" + "\n" + boloes;
+    }
+
+    public int getPontos() {
+        return pontos;
+    }
+
+    public void setPontos(int pontos) {
+        this.pontos = pontos;
+    }
+
+    public ArrayList<Aposta> getApostas() {
+        return apostas;
+    }
+
+    public ArrayList<Bolao> getBoloes() {
+        return boloes;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public void imprimir() {
+        System.out.println(this.toString());
+    }
 }
--- javabet/src/Usuario.java
+++ javabet/src/Usuario.java
@@ -1,8 +1,7 @@
 public abstract class Usuario {
-<<<<<<< HEAD
-
-=======
     private String email;
     private String senha;
->>>>>>> 2dec6d96404176c08215b5675f7e838375905df6
+
+    public Usuario(String email, String senha) {
+    }
 }
