5119b40d0870c73fd3adb3323dbce47ea344e267
Gabriel Kleiman <kleim4n@gmail.com>
2023-02-11 22:14:56 -0300

    CL01 UPDATE: Adicionado getters e setters

--- javabet/src/Usuario.java
+++ javabet/src/Usuario.java
@@ -6,4 +6,20 @@ public abstract class Usuario {
         this.email = email;
         this.senha = senha;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
