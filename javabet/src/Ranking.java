77054a06a868b0c8ad7c1970623b1c1b8144125e
OmegaTopo <74385000+OmegaTopo@users.noreply.github.com>
2023-02-12 21:01:38 -0300

    [CL] Alterações nos métodos da classe Ranking

--- javabet/src/Ranking.java
+++ javabet/src/Ranking.java
@@ -2,27 +2,39 @@ import java.util.*;
 
 public class Ranking implements Imprimir{
 
-    private List<Apostador> apostadores = new ArrayList<>();
+    private List<Apostador> apostadores;
+    private List<Jogo> jogos;
 
     public Ranking(List<Apostador> apostadores) {
         this.apostadores = apostadores;
     }
-
     public Ranking(){}
 
-    public int getPontuacaoGeral(){
-        return 0;
+    public ArrayList<Apostador> getPontuacaoGeral(){
+        List<Apostador> listaGeral = apostadores.stream()
+            .sorted(Comparator.comparing(Apostador::getPontos))
+            .toList();
+        System.out.println(listaGeral);
+        return getPontuacaoGeral();
     }
 
-    public int getPontuacaoCampeonato(){
-        return 0;
+    public ArrayList<Jogo> getPontuacaoCampeonato(){
+        for ()
+        System.out.println(listaCampeonato);
+        return getPontuacaoCampeonato();
     }
 
     public int getPontuacaoPais(){
+        getPontuacaoGeral();
+        List<Jogo> listaPais = jogos.stream()
+            .sorted(Comparator.comparing(Jogo::getPais));
+            .toList();
+        System.out.println(listaCampeonato);
         return 0;
     }
 
     @Override
     public void imprimir() {
+        System.out.println("");
     }
 }
