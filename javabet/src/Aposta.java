77e12f20636f0503ada3fb53a775600df393ce48
Gabriel Kleiman <kleim4n@gmail.com>
2023-02-13 04:15:00 -0300

    CL: Aposta reorganizado código e tirado comentários

--- javabet/src/Aposta.java
+++ javabet/src/Aposta.java
@@ -3,7 +3,6 @@ public class Aposta implements Premio {
 
     private Jogo jogo;
     private String previsaoTime;
-//    Talvez o ideal seja previsaoPlacar inicial como {-1, -1}, assim ficará claro que não foram feitos palpites no placarExato
     private int[] previsaoPlacar = {0,0};
     private boolean finalizado;
     final static int PONTOS_PLACAR_EXATO = 10;
@@ -56,7 +55,6 @@ public class Aposta implements Premio {
 
     @Override
     public boolean distribuirPremio(Apostador apostador) {
-//      Caso o jogo tenha sido finalizado e a aposta não: define o fim da aposta, recebe o resultado e incrementa na pontuação do apostador
         if (this.getJogo().isFinalizado() && !this.getFinalizado()){
             this.setFinalizado(true);
             apostador.setPontos(apostador.getPontos() + this.getResultado());
@@ -65,7 +63,6 @@ public class Aposta implements Premio {
         return false;
     }
     public boolean distribuirPremio(Bolao bolao) {
-//      Caso o jogo tenha sido finalizado e a aposta não: define o fim da aposta, recebe o resultado e incrementa na pontuação do bolãor
         if (this.getJogo().isFinalizado()){
             this.setFinalizado(true);
             bolao.setPontos(bolao.getPontos() + this.getResultado());
@@ -81,13 +78,10 @@ public class Aposta implements Premio {
 
     @Override
     public int getResultado() {
-//        O retorno automático de uma aposta em jogo em andamento é ZERO
         if (this.getJogo().isFinalizado()){
             String timeVencedor = "EMPATE";
-//            Primeiro é verificado se o palpite de placar bate com o do Jogo
-//            System.out.println(this.getPrevisaoPlacar()[0] + ":" + this.getPrevisaoPlacar()[1] + " " + this.getJogo().getPlacar() + " " + (this.getPrevisaoPlacar() == this.getJogo().getPlacar()));
-//            if (this.getPrevisaoPlacar() == this.getJogo().getPlacar()){     //Não funciona pois compara os endereções de memória
-            if ((this.getPrevisaoPlacar()[0] == this.getJogo().getPlacar()[0]) && (this.getPrevisaoPlacar()[1] == this.getJogo().getPlacar()[1])){
+            if ((this.getPrevisaoPlacar()[0] == this.getJogo().getPlacar()[0]) &&
+                    (this.getPrevisaoPlacar()[1] == this.getJogo().getPlacar()[1])){
                 return PONTOS_PLACAR_EXATO;
             } else if (this.getJogo().getPlacar()[0] > this.getJogo().getPlacar()[1]) {
                 timeVencedor = this.getJogo().getTimes()[0];
